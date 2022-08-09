d758289098eb900cd93b411e17b8d76e6e88a5c9
Sharon <sharonhw888@gmail.com>
2022-08-09 10:27:55 -0500

    create two components

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
 
 import React from "react";
+import AddCollection from "./Components/addCollection.js";
+import GalleryDisplay from "./Components/galleryDisplay.js";
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      userName='Jane'
+    }
+  }
   render() {
     const { name } = this.props;
     return (
       <>
         <h1>
-          Hello {name}, hello sharon
+          Hello {name}. Nice to see you here.
         </h1>
+        <AddCollection/>
+        <GalleryDisplay/>
       </>
     );
   }
---
+++ client/src/Components/galleryDisplay.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+var cloudName = 'dls2rxfqj';
+var presetName = 'ap4g9ume';
+
+var GalleryDisplay=(props)=>{
+  return(
+    <div>
+    </div>
+  )
+}
+
+export default GalleryDisplay
